2e0f33c150552165964a0808a3a77256ff2c9a7d
Brandon Nguyen <brandontrinhnguyen@gmail.com>
2017-03-30 03:54:45 -0500

    more drivers yay!

---
+++ src/drivers/devices/Fingers.h
@@ -0,0 +1,45 @@
+#ifndef __FINGERS_H__
+#define __FINGERS_H__
+/**
+ * @file    Fingers.h
+ * @author  Brandon Nguyen
+ * @brief   Driver for the flex sensors, exposed as fingers
+ */
+
+#include <stdint.h>
+
+typedef enum Finger_ID_enum
+{
+  Finger_Thumb = 0,
+  Finger_Index,
+  Finger_Middle,
+  Finger_Ring,
+  Finger_Pinky
+} Finger_ID;
+
+/**
+ * @struct    Fingers
+ * @brief     Holds finger data
+ */
+typedef struct Fingers_str
+{
+  uint32_t  timestamp;
+  int8_t    flexion[5];
+  int8_t    abduction[1];
+} Fingers;
+
+/**
+ * @fn          Finger_Init
+ * @brief       Initializes the Finger
+ */
+void Finger_Init(void);
+
+/**
+ * @fn          Finger_GetReadings
+ * @brief       Gets readings from the Finger
+ * @param[out]  readings  Place to write the readings to, updating its timestamp
+ */
+void Finger_GetReadings(Finger_Data * readings);
+
+
+#endif//__FINGERS_H__
---
+++ src/drivers/tm4c/ADC.h
@@ -0,0 +1,39 @@
+#ifndef __ADC_H__
+#define __ADC_H__
+/**
+ * @file    ADC.h
+ * @author  Brandon Nguyen
+ * @brief   Driver for the TM4C ADC
+ */
+
+#include <stdint.h>
+
+#define ADC_SIZE 6
+
+/**
+ * @struct    ADC_Data
+ * @brief     Holds data about ADC and completion status
+ */
+typedef struct ADC_Data_str
+{
+  int flag;
+  uint16_t values[ADC_SIZE];
+} ADC_Data;
+
+/**
+ * @fn        ADC_Init
+ * @brief     Initializes the ADC
+ */
+void ADC_Init(void);
+
+/**
+ * @fn      ADC_BeginRead
+ * @brief   Kicks off a full ADC read
+ *
+ * Concurrent implementation. Clears the flag and
+ * when complete, flag in data will be set.
+ */
+void ADC_BeginRead(ADC_Data * data);
+
+
+#endif//__ADC_H__
